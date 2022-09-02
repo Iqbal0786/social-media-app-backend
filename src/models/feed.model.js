03b27a9dadc62863d27cd9346439ef24e4a35310
Mohammad Iqbal <iqbal240456@gmail.com>
2022-09-02 20:23:18 +0530

    added feed schema

---
+++ src/models/feed.model.js
@@ -0,0 +1,19 @@
+const mongoose= require("mongoose");
+
+ // creating  feed  schema
+ 
+    const feedSchema= new mongoose.Schema({
+        title:{type:String , required:true},
+        profile_pic:[{type:String}],
+        user_id:{
+            type:mongoose.Schema.Types.ObjectId , ref:"user",required:true
+        },
+       
+  
+    },{
+        versionKey:false,
+        timestamps:true
+    });
+  
+    // creating the post model means posts collections 
+    module.exports= mongoose.model("post",feedSchema);
